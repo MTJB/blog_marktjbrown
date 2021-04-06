d823ca7b149720106c57286c9873954b86e0f972
MTJB <markbrown04@googlemail.com>
2021-04-06 14:14:09 +0100

    Add logo, remove dark toggle, default dark theme

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+/**
+ * Paste in your SVG logo and return it from this component.
+ * Make sure you have a height set for your logo.
+ * It is recommended to keep the height within 25-35px.
+ * Logo comes with a property value called `fill`. `fill` is useful 
+ * when you want to change your logo depending on the theme you are on. 
+ */
+export default function Logo({ fill }) {
+  return (
+    <svg fill={fill} version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="230px" height="70px" viewBox="0 0 230 170" enable-background="new 0 0 230 170" xmlSpace="preserve"> 
+    <image id="image0" width="230" height="170" x="0" y="0"
+    xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOYAAACqCAMAAACOLsOeAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+                AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAPFBMVEUAAAC/JlGvI1LHV3jf
+                7e3UqrnXu8bRepTTjaPYkafcg57caYrbPWrRNWHILVm/JlGRHVXf7e3bPWr///8dRz/zAAAAD3RS
+                TlMAAAAAAAAAAAAAAAAAAAAnp8O0AAAAAWJLR0QTDLtclgAAAAd0SU1FB+UEBg4FBsQ8pfcAAAEJ
+                SURBVHja7dzLEoIgGIDRLLvfe/+HbfezQUcnwJrOt0REzo4F42olSZIkSZIkSZIk/XH30R7RfaG6
+                CBMTExMTExOzDjMtuB6taxAmJiYmJiYmZjvmJuoXaptpF6V5mJiYmJiYmJglmfvoObu0hanzcm9M
+                XQUTExMTExMT83PmKzpE4x8eH5v6dP7KmJiYmJiYmJglmemwd4y+iZkLExMTExMTE/NzZjrstWCW
+                GcPExMTExMTELMk8RS2YuabOw8TExMTExMQsyTxH/UJVvASOiYmJiYmJiTnIvFTrmumWqeIPATAx
+                MTExMTExB5ktStvPjeXCxMTExMTExKzDlCRJkiRJkiRJkn68N8rWBToFfboGAAAAJXRFWHRkYXRl
+                OmNyZWF0ZQAyMDIxLTA0LTA2VDE0OjA1OjA2KzAzOjAwa7e8RAAAACV0RVh0ZGF0ZTptb2RpZnkA
+                MjAyMS0wNC0wNlQxNDowNTowNiswMzowMBrqBPgAAAAASUVORK5CYII=" 
+    />
+</svg>
+  );
+}
